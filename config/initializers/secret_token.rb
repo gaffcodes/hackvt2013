7b83cc5e6776eb63b8b96789014f20de5b200251
Aaron Lipman <alipman88@gmail.com>
2013-10-11 18:59:48 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VtPoll::Application.config.secret_key_base = '1c2770068593d7f116830285d79f66d7b81e3b2b1cf44af6547f0e23ffd69d3ffb667de7cbb876e01186d0fa8265219fb8641b64db6964a3fe636481dae987b1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VtPoll::Application.config.session_store :cookie_store, key: '_vt_poll_session'
